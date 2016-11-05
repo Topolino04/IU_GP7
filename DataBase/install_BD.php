7583fd3cfea6eb6cc959b8e11f4f8cdc067dfe31
mpugaeg <martin_universidad@Martins-iMac.home>
2016-11-05 23:08:21 +0100

    Revert "Merge branch 'master' into Martin"
    This reverts commit 90eed18b89777f06eae13414e8201132e346b3b4, reversing
    changes made to b9aac6bfbd0cdbe029020bf02fe134ccfa1ed7c7.

--- DataBase/install_BD.php
+++ DataBase/install_BD.php
@@ -11,24 +11,12 @@ if ($conn->connect_error) {
 }
 
 // Create database
-$sql = "DROP DATABASE IU_DATABASE";
-$conn->query($sql);
-$sql = "CREATE DATABASE IU_DATABASE";
+$sql = "CREATE DATABASE myDB";
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully";
-    ejecutarSQL("DataBase.sql",$conn);
 } else {
     echo "Error creating database: " . $conn->error;
 }
 
 $conn->close();
-function ejecutarSQL($_rutaArchivo, $_conexionDB){
-    $queries = explode(';', file_get_contents($_rutaArchivo));
-    foreach($queries as $query){
-        if($query != ''){
-            $_conexionDB->query($query); // Asumo un objeto conexión que ejecuta consultas
-        }
-    }
-}
-
 ?>
