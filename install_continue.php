facddd3a2728b6762411bfcdd330c63aa59b08ed
Martin (Universidad) <Universidad@Martins-MacBook-Pro.local>
2016-11-24 20:57:38 +0100

    Installers V0.1

--- install_continue.php
+++ install_continue.php
@@ -1,25 +1,13 @@
 <?php
-mysqli_connect("localhost", $_POST["usuario"], $_POST["password"]) or die ("Usuario o contraseña no son root en la base de datos");
-
-if (!$enlace) {
-    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-
-
-
 
 $mysqli = new mysqli("localhost", $_POST["usuario"], $_POST["password"]);
 
 
 if ($mysqli->connect_errno) {
-   // echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+   echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else {
     $sql = '';
-    $fichero = file(“/IU2016.sql”);
+    $fichero = file('./et1_iu/IU2016.sql');
     foreach ($fichero as $linea) {
         if (substr($linea, 0, 2) === '--' || $linea === '') {
             continue;
